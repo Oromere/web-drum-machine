f856f71a45d0c815ab0feb8a455feef1127ac2bf
Florian <flo.schlueter.dev@gmail.com>
2020-09-10 18:21:43 +0200

    add new samples

--- src/assets/sounds.js
+++ src/assets/sounds.js
@@ -1,27 +1,39 @@
-import { kick, snare, hihatOpen, hihatClose } from "./samples";
+import { kick, snare, hihatOpen, hihatClose, clap, tom } from "./samples";
 export const sounds = [
   {
     id: 1,
     number: 0,
-    name: "kick",
+    name: "Kick",
     src: kick,
   },
   {
     id: 2,
     number: 1,
-    name: "snare",
+    name: "Snare",
     src: snare,
   },
   {
     id: 3,
     number: 2,
-    name: "closed hihat",
+    name: "Closed hihat",
     src: hihatClose,
   },
   {
     id: 4,
     number: 3,
-    name: "open hihat",
+    name: "Open Hihat",
     src: hihatOpen,
   },
+  {
+    id: 5,
+    number: 4,
+    name: "Clap",
+    src: clap
+  },
+  {
+    id: 6,
+    number: 5,
+    name: "Tom",
+    src: tom
+  }
 ];
